08f361910eecb4089410f3d7a1b1b872fd4651ea
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2021-09-22 18:31:01 -0300

    Creating a form to create a new propety

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,4 +2,17 @@ class PropertiesController < ApplicationController
     def show
         @property = Property.find(params[:id])
     end
+
+    def new
+        @property = Property.new()
+    end
+    
+    def create
+
+        property_params = params.require(:property).permit(:title, :description, :rooms, :parking_slot, :bathrooms, :pets, :daily_rate)
+
+        property = Property.create(property_params)
+        
+        redirect_to property_path(property.id)
+    end
 end
